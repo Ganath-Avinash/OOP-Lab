39dd7b2112803e4a1ab67c442bf3b61d77ff7f40
Ganath-Avinash <ganathavinash.gr@gmail.com>
2025-04-07 23:56:59 +0530

    Add files via upload

---
+++ Exercise 9/Eh.java
@@ -0,0 +1,24 @@
+ 
+import java.util.Scanner; 
+public class Eh { 
+    public void divide(int a, int b) throws ArithmeticException { 
+        int result= a / b; 
+    } 
+ 
+    public static void main(String[] args) { 
+        try { 
+   	Scanner ip=new Scanner(System.in); 
+   	int a=ip.nextInt(); 
+   	int b=ip.nextInt(); 
+   	EH2 e1= new EH2(); 
+   	e1.divide(a,b); 
+          System.out.println("Result: " + a/b); 
+        }  
+ 	catch (ArithmeticException e) { 
+          System.out.println("Error: Division by zero is not allowed."); 
+        } 
+	catch (Exception er) { 
+          er.printStackTrace(); 
+        } 
+    } 
+}
